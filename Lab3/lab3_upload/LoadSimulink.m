1ae3e8f24925dfbdec1a1db1809acf6ec2dff01e
Lorenzo Grottesi <s268978@studenti.polito.it>
2021-01-20 15:57:58 +0100

    doc: end part 1 LAB 3

--- Lab3/lab3_upload/LoadSimulink.m
+++ Lab3/lab3_upload/LoadSimulink.m
@@ -74,47 +74,83 @@ VOC =  BATT_C2_INT + R*I2 ;
 % p4 = 11.22
 % p5 = 0.5787
 clearvars BATT_C1 BATT_C2 C I1 I2 BATT_C1_INT BATT_C2_INT
+
 %% BATTERY PLOT.
+figure (1);
 time = 1:length(battSOC);
 total_time = length(time)/3600
 plot(time, battSOC);
 hold on
 plot(time, battVoltage);
-hold on 
-plot(time, battCurrent);
 xlabel ('Time [s]');
-legend('SOC', 'BatteryVoltage', 'BatteryCurrent');
-%% LOAD PLOT
-time = 1:length(battSOC);
-plot(time, powerToDC);
-hold on
-plot(time, 3.3*loadCurrent);
-xlabel ('Time [s]');
-legend('PowerLoads and PVPower difference', 'LoadPower');
+legend('SOC', 'BatteryVoltage');
+figure(2);
+subplot(2, 1, 1);
+plot(time(1:period), efficiencyBattery(1:period));
+xlabel('Time [s]');
+ylabel('Efficiency');
+title('Battery DC-DC efficiency in time');
+subplot(2, 1, 2);
+plot(IRequest(1:period), efficiencyBattery(1:period));
+xlabel('I Load [A]');
+ylabel('Efficiency');
+title('Battery DC-DC efficiency in current');
 %% PV I-V, power, efficiency.
 time = 1:length(battSOC);
 %I
 figure(1);
+subplot(2,2,1);
+plot(time, LIGHT_WL);
+xlabel ('Time [s]');
+ylabel ('Light');
+title('PV Light');
+subplot(2,2,2);
 plot(time, PVcurrent);
 xlabel ('Time [s]');
 ylabel ('PV current [A]');
+title('PV Current');
 %V
-figure(2);
+subplot(2,2,3);
 plot(time, PVvoltage);
 xlabel ('Time [s]');
 ylabel ('PV voltage [V]');
+title('PV Voltage');
 %P
-figure(3);
+subplot(2,2,4);
 PVpower = PVcurrent.*PVvoltage;
 plot(time, PVpower);
 xlabel ('Time [s]');
 ylabel ('PV watt [W]');
-figure(4);
+title('PV Power');
+figure(2);
 plot(PVvoltage, efficiencyPV);
 xlabel ('PV voltage [V]');
 ylabel ('Efficiency');
-%%
+%% ACTIVE PV.
+time = 1:length(battSOC);
+plot(time, activePV);
+xlabel('time');
+ylabel('0 => PV not charging, 1 => PV charging');
+title('PV active');
+%% LOAD PLOT
+subplot(2,1,1);
 time = 1:(2*period);
+plot (time, 3.3*airI(1:2*period));
+hold on 
+plot (time, 3.3*methI(1:2*period));
+hold on 
+plot (time, 3.3*micI(1:2*period));
+hold on 
+plot (time, 3.3*tempI(1:2*period));
+hold on 
+plot (time, 3.3*mcI(1:2*period));
+hold on 
+plot (time, 3.3*zbI(1:2*period));
+legend('AIR', 'METH', 'MIC', 'TEMP', 'MAC', 'TRAN');
+xlabel('Time [s]');
+ylabel('Power [w]');
+title('Load power request');
+subplot(2,1,2);
 plot (time, AIR_PERIOD(1:2*period));
 hold on 
 plot (time, METHANE_PERIOD(1:2*period));
@@ -127,3 +163,5 @@ plot (time, MAC_PERIOD(1:2*period));
 hold on 
 plot (time, TRANSMIT_PERIOD(1:2*period));
 legend('AIR', 'METH', 'MIC', 'TEMP', 'MAC', 'TRAN');
+xlabel('Time [s]');
+title('Workload');
